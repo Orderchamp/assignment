cf86f5cd76c7a1b05840bd8c7d361df5c1f6c45d
Anderson Mamede <anderson@digifit.eu>
2021-10-12 17:56:21 +0200

    Create CartItemFactory.php

---
+++ database/factories/CartItemFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Cart;
+use App\Models\CartItem;
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CartItemFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = CartItem::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $product = Product::factory()->create();
+
+        return [
+            'cart_id' => Cart::factory()->create(),
+            'product_id' => $product->id,
+            'price' => $product->price,
+            'quantity' => $this->faker->randomDigit(),
+        ];
+    }
+}
